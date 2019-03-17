6e77c15618df089ecc3f72c6f1999d588ecd0bdf
Hasan  Yılmaz <33355796+hsnylmzz@users.noreply.github.com>
2019-03-17 12:44:50 +0300

    Create alfabetik siralama2.c

---
+++ alfabetik siralama2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define size 50
+void sortalphabet(char *s);
+int main() {
+  char a[50]="osmanyilmaz";
+  sortalphabet(a);
+  return 0;
+}
+void sortalphabet(char *s)
+{
+  int i,j;
+  char temp;
+  for(i=0;s[i]!='\0';i++)
+  {
+    for(j=0;s[j]!='\0';j++)   // Bu kodda for içine j=i+1 değilde j=0 konulursa Z den A ya kadar sıralar.
+    {
+      if(s[i]>s[j])
+      {
+        temp=s[j];
+        s[j]=s[i];
+        s[i]=temp;
+      }
+    }
+  }
+  printf("Alfabetik Siralanmis Hali\n");
+  for(i=0;s[i]!='\0';i++)
+  {
+    printf("%c",s[i]);
+  }
